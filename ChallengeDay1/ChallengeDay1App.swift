3b2c76b6bd208c4e8229f6c178b8e2e254c7b068
Ceren <ctassin@geobilgi.com>
2022-01-21 21:01:26 +0300

    Initial Commit

---
+++ ChallengeDay1/ChallengeDay1App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay1App.swift
+//  ChallengeDay1
+//
+//  Created by Ceren TAŞSIN on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
